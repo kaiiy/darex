71daf1f5ea7945bcb9fb6f5e1947e41d5bda66eb
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-03-02 21:39:12 +0900

    Initial commit

---
+++ test/darex.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Darex from '../lib/darex-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/darex-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Darex.DarexStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
